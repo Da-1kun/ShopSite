772d6b2ce1f547423705c89f43f18c4d1e436676
daichi <wjsn.cosmic94@gmail.com>
2021-02-12 10:20:41 +0900

    revise settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .settings_local import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6k3!xy+7u*#yeh80_6#8(mct%(-g)!cmk7qu_z=060s_8nme8n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
